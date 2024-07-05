181f9c0d259160fa400e809321999d12093d506f
rb5883 <rb58853@gmail.com>
2024-07-05 18:33:37 -0400

    save

--- src/components/chat/chatComponents/message/waitMessage.jsx
+++ src/components/chat/chatComponents/message/waitMessage.jsx
@@ -2,9 +2,13 @@ import './styles/desktop.css'
 function WaitMessage() {
     return (
         <div className="waitMessage">
-
-            <img className='waitMessageFloat' src="/gifs/waitMessage.gif" alt="connecting"
-                style={{ width: "70px", height: "70px", placeSelf: 'center', alignSelf: 'center' }} />
+            {/* <img className='waitMessageFloat' src="/gifs/messages-typing.gif" alt="connecting" /> */}
+            <div>
+                <img className='waitMessageFloat other' src="/gifs/waitMessage.gif" alt="connecting" />
+                <img className='waitMessageFloat other' src="/gifs/waitMessage.gif" alt="connecting" />
+                <img className='waitMessageFloat other' src="/gifs/waitMessage.gif" alt="connecting" />
+                <img className='waitMessageFloat other' src="/gifs/waitMessage.gif" alt="connecting" />
+            </div>
         </div>
     )
 }
